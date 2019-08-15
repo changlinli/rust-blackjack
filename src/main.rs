8ad085298b5c843c5bf47bcacd2edd8ecc2dc816
Changlin Li <mail@changlinli.com>
2019-08-15 15:24:14 -0400

    Begin work on blackjack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+use rand::Rng;
+
+enum Action {
+    Hit,
+    Stand,
+    DoubleDown,
+    SplitCards,
+    Surrender
+}
+
+fn parse_action(str: &String) -> Action {
+    let result = match str.trim() {
+        "hit" => Action::Hit,
+        "stand" => Action::Stand,
+        "double-down" => Action::DoubleDown,
+        "split" => Action::SplitCards,
+        "surrender" => Action::Surrender,
+        _ => Action::Hit,
+    };
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Play blackjack!");
+
+    println!("Please input what you'd like to do (hit/stand/double-down/split/surrender):");
+
+    let mut raw_action = String::new();
+
+    let action = parse_action(&raw_action);
+
+    let card = rand::thread_rng().gen_range(1, 101);
+
+    io::stdin().read_line(&mut raw_action).expect("Failed to read line!");
+
+    println!("You wanted to: {}", raw_action);
 }
